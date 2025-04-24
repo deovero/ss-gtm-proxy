59c2794125f10e0fe794de6e29bb08f53bd45a0c
Jeroen Vermeulen <info@jeroenvermeulen.eu>
2025-04-24 09:59:46 +0200

    Updated config for multi-domain

--- config.example.php
+++ config.example.php
@@ -1,5 +1,19 @@
 <?php
 
-return [
-    'gtm_url' => 'https://gtm-docker-server.domain.com/prefix'
-];
+$siteDomain = $_SERVER['HTTP_HOST'] ?? '';
+$siteDomain = preg_replace('/^www\./', '', $siteDomain);
+
+switch ($siteDomain) {
+    case 'domain-one.com':
+        return [
+            'gtm_url' => 'https://gtm-docker-server.domain.com/prefix-one'
+        ];
+    case 'domain-two.com':
+        return [
+            'gtm_url' => 'https://gtm-docker-server.domain.com/prefix-two'
+        ];
+    default:
+        return [
+            'gtm_url' => 'https://gtm-docker-server.domain.com/prefix-x'
+        ];
+}
